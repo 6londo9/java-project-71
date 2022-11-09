4324314f13c51bbe78c3af8f349b876177fd93d4
Ilya <6londo9@gmail.com>
2022-11-09 20:50:41 +1100

    try add Differ method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,40 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+class App implements Callable<Integer> {
+
+    private static final int SUCCES_EXIT_CODE = 0;
+    private static final int ERROR_EXIT_CODE = 1;
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Override
+    public final Integer call() {
+        try {
+            System.out.println(Differ.generate(filepath1, filepath2));
+        } catch (Exception e) {
+            return ERROR_EXIT_CODE;
+        }
+        return SUCCES_EXIT_CODE;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
