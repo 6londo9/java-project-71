034702a1b99c33d5cfdb6474514073fb6bbedc20
Ilya <6londo9@gmail.com>
2022-11-20 17:02:02 +1100

    delete unused imports from AppTest

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import hexlet.code.Utils.Formatter;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
